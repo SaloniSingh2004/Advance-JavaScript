60347916ae3a186a62fe5889548f9c8f56039d05
SaloniSingh2004 <saloni.pingi222@gmail.com>
2025-04-09 16:01:35 +0530

    let vs var

--- script.js
+++ script.js
@@ -40,18 +40,149 @@
 // let result=addTwoNumbers(4,5);
 // console.log(result);
 
-function addTwoNumbers(x,y){
-    return x+y;
-}
-let resultArr=[];
-for(let i=0;i<10;i++){
-    let result=addTwoNumbers(i,2*i);
-    resultArr.push(result);
-}
-console.log(resultArr);
+// function addTwoNumbers(x,y){
+//     return x+y;
+// }
+// let resultArr=[];
+// for(let i=0;i<10;i++){
+//     let result=addTwoNumbers(i,2*i);
+//     resultArr.push(result);
+// }
+// console.log(resultArr);
 
 //Returning with arrow function
 
 // let addTwoNumbers = (x,y) => x+y;
 // let result=addTwoNumbers(4,5);
 // console.log(result);
+
+//Variable scope in function
+
+// function testavailability(x){
+//     console.log("Available here:",x);
+// }
+// testavailability("hi!");
+// console.log("Not available here:",x);
+
+// function testavailability(){
+//     let y="Local Variable!";
+//     console.log("Available here:",y);
+// }
+// testavailability();
+// console.log("Not Available here:",y);
+
+// function testavailability(){
+//     let y="i'll return";
+//     console.log("Available here:",y);
+//     return y;
+// }
+// let z=testavailability();
+// console.log("Outside the function:",z);
+// console.log("Not Available here:",y);
+
+// function doingstuff(){
+//     if(true){
+//         var x="local";
+//     }
+//     console.log(x);
+// }
+// doingstuff();
+
+// function doingstuff(){
+//     if(true){
+//         let x="local";
+//     }
+//     console.log(x);
+// }
+// doingstuff();
+
+// function doingstuff(){
+//     if(true){
+//         console.log(x);
+//         let x="local";
+//     }
+// }
+// doingstuff();
+
+//Constant scope
+
+// function doingstuff(){
+//     if(true){
+//         const x="local";
+//     }
+//     console.log(x);
+// }
+// doingstuff();
+
+//Global Variable
+
+// let globalvar="accessible everywhere!";
+// console.log("Outside The function:",globalvar);
+
+// function creatingNewscope(x){
+//     console.log("Access to global vars inside function:",globalvar);
+// }
+// creatingNewscope("some parameter");
+// console.log("Still available:",globalvar)
+
+// let x="global";
+// function doingstuff(){
+//     let x="local";
+//     console.log(x);
+// }
+// doingstuff();
+// console.log(x);
+
+// let x="global";
+// function doingstuff(){
+//     x="local";
+//     console.log(x);
+// }
+// doingstuff();
+// console.log(x);
+
+// let x="global";
+// function doingstuff(x){
+//     console.log(x);
+// }
+// doingstuff("param");
+
+// function confusereader(){
+//     x="Guess my scope...";
+//     console.log("Inside the function:",x);
+// }
+// confusereader();
+// console.log("Outside the function:",x);
+
+//Immediately invoked function expression
+// The immediately invoked function expression(IIFE) is a way of expressing function so that it gets invoked immediately
+
+// (function (){
+//     console.log("IIFE!");
+// })();
+
+// (()=>{
+//     console.log("run right away");
+// })();
+
+//Recursive function
+
+// function getrecursive(nr){
+//     console.log(nr);
+//     if(nr>0){
+//         getrecursive(--nr);
+//     }
+// }
+// getrecursive(3);
+
+function getrecursive(nr){
+    console.log("Started function:",nr);
+    if(nr>0){
+        getrecursive(nr-1);
+    }
+    else{
+        console.log("done with recursion");
+    }
+    console.log("Ended function:",nr);
+}
+getrecursive(3);
