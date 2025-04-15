bc75e90cddbc1b455f3cf7efecbdd8238d9ca21d
SaloniSingh2004 <saloni.pingi222@gmail.com>
2025-04-15 13:10:31 +0530

    recursive function

--- script.js
+++ script.js
@@ -175,14 +175,87 @@
 // }
 // getrecursive(3);
 
-function getrecursive(nr){
-    console.log("Started function:",nr);
-    if(nr>0){
-        getrecursive(nr-1);
-    }
-    else{
-        console.log("done with recursion");
-    }
-    console.log("Ended function:",nr);
-}
-getrecursive(3);
+// function getrecursive(nr){
+//     console.log("Started function:",nr);
+//     if(nr>0){
+//         getrecursive(nr-1);
+//     }
+//     else{
+//         console.log("done with recursion");
+//     }
+//     console.log("Ended function:",nr);
+// }
+// getrecursive(3);
+
+// function getrecursive(nr){
+//         if(nr>0){
+//             getrecursive(--nr);
+//         }
+//         console.log(nr);
+// }
+// getrecursive(3);
+
+//The performance of recursion is slightly worse than the performance of regular iteration using a loop. So it causes a bottleneck situation that would really slow down 
+
+//Nested Functions
+
+// function doouterfunctionstuff(nr){
+//     console.log("Outer function");
+//     doinnerfunctionstuff(nr);
+//     function doinnerfunctionstuff(x){
+//         console.log(x+7);
+//         console.log("I can access the outer variables:",nr);
+//     }
+// }
+// doouterfunctionstuff(2);
+
+//Anonymous functions
+
+// let functionvariable=function(){
+//     console.log("Not so secret though.");
+// };
+
+//Like passing a function as parameters. This concept adds another abstract layer to the coding. This concept is called callbacks
+
+//Function Callbacks
+
+// function doflexiblestuff(executestuff){
+//     executestuff();
+//     console.log("Inside doflexibleStufffunction");
+// }
+// doflexiblestuff(functionvariable);
+
+//In JavaScript there are many built in functions as you may know by now. One of them is the setTimeout() function, It is a very special function that is executing a certain function after a specified amount of time that it will wait first
+
+// let youGotthis=function(){
+//     console.log("You are doing really well, keep coding!");
+// };
+// // setTimeout(youGotthis,1000);
+// setInterval(youGotthis,1000);
+
+//If you need more encouragement you can use the setInterval() function instead it works very similarly, but instead of executing the specified function once it will keep on executing 
+
+// let testfunction=function(){
+//     console.log("Hello");
+// }();
+
+// (function(){
+//     console.log("Welcome");
+// })();
+// (function(){
+//     let firstname="Laurence";
+// })();
+// let result=(function(){
+//     let firstname="Laurence";
+//     return firstname;
+// })();
+// console.log(result);
+// (function(firstname){
+//     console.log("My Name is "+firstname);
+// })("Laurence");
+
+// var addfive1=function addfive1(num){
+//     return num+2;
+// };
+// let addfive2=(num)=>num+2;
+// console.log(addfive1(14));
