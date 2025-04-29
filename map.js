ca7f4bf99489380c5e0c17da60c040e2daf35f9d
SaloniSingh2004 <saloni.pingi222@gmail.com>
2025-04-29 12:57:17 +0530

    Rest Property trailing commas

--- map.js
+++ map.js
@@ -109,7 +109,7 @@
 // console.log(age);
 // console.log(country);
 
-//Alias Object Property
+// Alias Object Property
 // const person={
 //     firstname:"Saloni",
 //     lastname:"Singh",
@@ -121,3 +121,77 @@
 // console.log(name);
 // console.log(age);
 // console.log(country);
+// console.log(lastname);
+
+// let name="GurukulTheSchools";
+// let[a1,a2,a3,a4,a5]=name;
+// console.log(a1,a2,a3,a4,a5);
+
+// const fruits=["Bananas","Oranges","Apples","Mangoes"];
+// let[fruit1,fruit2]=fruits;
+// console.log(fruit1,fruit2);
+
+// const fruits=["Bananas","Oranges","Apples","Mangoes"];
+// let[fruit1,,,fruit2]=fruits;
+// console.log(fruit1,fruit2);
+
+// const fruits=["Bananas","Oranges","Apples","Mangoes"];
+// let{[3]:fruit1,[2]:fruit2}=fruits;
+// console.log(fruit1,fruit2);
+
+//Rest Property
+// const numbers=[10,20,30,40,50,60,70];
+// const[a,b,...rest]=numbers;
+// console.log(a,b,rest);
+
+// const fruits=new Map([
+//     ["apples",500],
+//     ["bananas",300],
+//     ["oranges",200]
+// ]);
+
+//Swapping the elements
+// let firstname="Saloni";
+// let lastname="Singh";
+// [firstname,lastname]=[lastname,firstname];
+// console.log(firstname,lastname);
+
+//JavaScript exponentiation
+// let x=5;
+// let z=x**2;
+// let z=Math.pow(x,2);
+// console.log(z);
+
+// let x=5;
+// x**=2;
+// console.log(x);
+
+//Array Includes
+// const fruits=["Bananas","Oranges","Apples","Mangoes"];
+// console.log(fruits.includes("Mangoes"));
+
+// const fruits=["Bananas","Oranges","Apples","Mangoes"];
+// console.log(fruits.includes("Bananas",3));
+
+//The include method is case sensitive,default is 0
+
+//Trailing Comma
+// const arr=[
+//     "one","two","three",,
+// ];
+// console.log(arr,arr.length);
+
+// const person={
+//     firstname:"Saloni",
+//     lastname:"Singh",
+//     age:20,
+// };
+// console.log(person);
+
+// const person={
+//     firstname:"Saloni",
+//     ,
+//     age:20,
+// };
+// console.log(person);
+
